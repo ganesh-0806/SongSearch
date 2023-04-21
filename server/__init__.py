706d894edc3e829452b02fe6d8e064cace11fab2
Ganesh Regoti <ganeshregoti@gmail.com>
2023-04-21 15:15:50 -0700

    Added pymongo client to connect to mongodb

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
 import os
+import yaml
+from pymongo import MongoClient
+
+db_file_path = os.path.join(os.path.dirname(__file__), 'db.yml')
+db_config = yaml.full_load(open(db_file_path))
+client = MongoClient()
+client = MongoClient('mongodb://'+db_config['host']+':'+db_config['port']+'/')
+db = client.Songcollection
 
 def create_app():
     app = Flask(__name__)
