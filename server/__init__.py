5274c98fd148c38092138fabcb84d03f1bbd6c09
clcubing <clcubing@gmail.com>
2023-04-21 17:21:11 -0700

    intermediate report version

--- server/__init__.py
+++ server/__init__.py
@@ -7,7 +7,7 @@ db_file_path = os.path.join(os.path.dirname(__file__), 'db.yml')
 db_config = yaml.full_load(open(db_file_path))
 client = MongoClient()
 client = MongoClient('mongodb://'+db_config['host']+':'+db_config['port']+'/')
-db = client.Songcollection
+db = client.testdb
 
 def create_app():
     app = Flask(__name__)
--- server/views.py
+++ server/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, jsonify
+from . import db
 import json
 
 views = Blueprint('views', __name__)
@@ -17,3 +18,87 @@ def home():
 
     return render_template('home.html')
     #return '<h1>hello</h1>'
+
+@views.route('/read-hardcoded', methods=['GET', 'POST'])
+def read_hardcoded():
+    if request.method == 'POST':
+        #Interact with db and get details
+        # jsonify the response
+        abc = True
+
+    response = '{response : 200}'
+    collection = db.Songcollection
+    result = []
+    for val in collection.find({"Title": "Pink World"}):
+       result.append(str(val))
+
+    return result
+
+@views.route('/create-hardcoded', methods=['GET', 'POST'])
+def create_hardcoded():
+    if request.method == 'POST':
+        #Interact with db and get details
+        # jsonify the response
+        abc = True
+
+    response = '{response : 200}'
+    collection = db.Songcollection
+    collection.insert_one({
+        "SongNumber": 10,
+        "SongID": "SOUDSGM12AC9618304",
+        "AlbumID": 692313,
+        "AlbumName": "Superinstrumental",
+        "ArtistID": "ARNTLGG11E2835DDB9",
+        "ArtistLatitude": "",
+        "ArtistLocation": "",
+        "ArtistLongitude": "",
+        "ArtistName": "Clp",
+        "Danceability": 0.0,
+        "Duration": 266.39628,
+        "KeySignature": 7,
+        "KeySignatureConfidence": 0.053,
+        "Tempo": 114.041,
+        "TimeSignature": 4,
+        "TimeSignatureConfidence": 0.878,
+        "Title": "Insatiable (Instrumental Version)",
+        "Year": 0
+    })
+    result = []
+    for val in collection.find():
+       result.append(str(val))
+
+    return result
+
+@views.route('/update-hardcoded', methods=['GET', 'POST'])
+def update_hardcoded():
+    if request.method == 'POST':
+        #Interact with db and get details
+        # jsonify the response
+        abc = True
+
+    response = '{response : 200}'
+
+    collection = db.Songcollection
+    collection.update_one({"SongID":"SOHKNRJ12A6701D1F8"}, {"$set" : {'Year': 1995}})
+    result = []
+    for val in collection.find():
+        result.append(str(val))
+
+    return result
+
+@views.route('/delete-hardcoded', methods=['GET', 'POST'])
+def delete_hardcoded():
+    if request.method == 'POST':
+        #Interact with db and get details
+        # jsonify the response
+        abc = True
+
+    response = '{response : 200}'
+
+    collection = db.Songcollection
+    collection.delete_one({"SongID": "SONHOTT12A8C13493C"})
+    result = []
+    for val in collection.find():
+        result.append(str(val))
+
+    return result
