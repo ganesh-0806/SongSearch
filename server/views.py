23c8bf677ee1e6195f339c52b50ae34023644f8d
clcubing <clcubing@gmail.com>
2023-05-02 16:27:40 -0700

    unhardcoded CRUD functions

--- server/views.py
+++ server/views.py
@@ -24,24 +24,25 @@ def read():
     if request.method == 'POST':
         #Interact with db and get details
         # jsonify the response
-        abc = True
-    song_id = request.form['view-song-id']
-    if not song_id:
-        return
-    response = '{response : 200}'
-    collection = db.Songcollection
-    result = []
-    for val in collection.find({song_id}):
-       result.append(str(val))
+        song_id = request.form['view-value']
+        if not song_id:
+            return
+        collection = db.Songcollection
+        result = []
+        for val in collection.find({song_id}):
+            result.append(str(val))
 
-    return result
+        return result
+
+    response = '{response : 200}'
+
 
 @views.route('/create/', methods=['GET', 'POST'])
 def create():
     print('create called')
     if request.method == 'POST':
         #Interact with db and get details
-        create_txt = request.form['create-text-area']
+        create_txt = request.form['create-value']
         print(create_txt)
         if not create_txt:
             return
@@ -62,40 +63,37 @@ def update():
     print('update called')
     if request.method == 'POST':
         #Interact with db and get details
-        # jsonify the response
-        abc = True
+        upd_id = request.form['update-id-value']
+        upd_text = request.form['update-text-value']
+        if not upd_id or not upd_text:
+            return
 
-    upd_id = request.form['update-song-id']
-    upd_text = request.form['update-text-area']
+        collection = db.Songcollection
+        collection.update_one({"SongID": upd_id}, {"$set": upd_text})
+        result = []
+        for val in collection.find():
+            result.append(str(val))
+
+        return result
     response = '{response : 200}'
 
-    if not upd_id or not upd_text:
-        return
 
-    collection = db.Songcollection
-    collection.update_one({"SongID": upd_id}, {"$set" : upd_text})
-    result = []
-    for val in collection.find():
-        result.append(str(val))
-
-    return result
 
 @views.route('/delete/', methods=['GET', 'POST'])
 def delete():
     print("delete called")
     if request.method == 'POST':
+        del_id = request.form['delete-value']
         #Interact with db and get details
         # jsonify the response
-        abc = True
-    del_id = request.form['delete-song-id']
-    response = '{response : 200}'
-    del_str = ""
-    if not del_id:
-        return
-    collection = db.Songcollection
-    collection.delete_one({"SongID": del_id})
-    result = []
-    for val in collection.find():
-        result.append(str(val))
+        if not del_id:
+            return
+        collection = db.Songcollection
+        collection.delete_one({"SongID": del_id})
+        result = []
+        for val in collection.find():
+            result.append(str(val))
 
-    return result
+        return result
+
+    response = '{response : 200}'
